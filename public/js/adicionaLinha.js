9f83121fb19a68ecb2749755e9d3e96fede5192e
Gufinger07 <gusti.cf@gmail.com>
2021-08-31 10:48:01 -0300

    Adicionando o evento de adicionar linha

---
+++ public/js/adicionaLinha.js
@@ -0,0 +1,16 @@
+const corpoTabela = $(".tabela-corpo")
+$(".botao-ajuda").click(adicionarLinha)
+var cont = 0006
+function adicionarLinha() {
+    cont += 1
+    const conteudo = `
+    <tr class="tabela-linha">
+              <th class="tabela-item" scope="row">000${cont}</th>
+              <td class="tabela-item">Trakinas</td>
+              <td class="tabela-item">R$ 7,00</td>
+              <td class="tabela-item">70 Unidades</td>
+            </tr>
+    `
+
+    corpoTabela.append(conteudo)
+}
