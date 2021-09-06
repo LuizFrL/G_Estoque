e709ea775410dd4d988e86859b9c338d2ca39d9b
Gufinger07 <gusti.cf@gmail.com>
2021-09-06 02:12:54 -0300

    primeira apresentação

--- public/js/adicionaLinha.js
+++ public/js/adicionaLinha.js
@@ -11,7 +11,13 @@ function novaLinha(nome, valor, qnt) {
   var itemNome = $("<td>").addClass("tabela-item").text(`${nome}`)
   var itemValor = $("<td>").addClass("tabela-item").text(`R$ ${valor},00 `)
   var itemQnt = $("<td>").addClass("tabela-item").text(`${qnt} Unidades`)
-  linha.append(itemId ,itemNome, itemValor, itemQnt)
+  var imagemPincel = $("<img>").attr("src", "../assets/svg/pencil.svg")
+  var imagemLixeira = $("<img>").attr("src", "../assets/svg/trash.svg")
+  var itemEditar = $("<td>").addClass("tabela-item")
+  var itemExcluir = $("<td>").addClass("tabela-item")
+  itemEditar.append(imagemPincel)
+  itemExcluir.append(imagemLixeira)
+  linha.append(itemId ,itemNome, itemValor, itemQnt, itemEditar, itemExcluir)
   corpo.append(linha)
 }
 
