f7590b2134da83a7ff03bc777f68a3281a38ac24
Luiz Fernando <luiz.fernando@zerum.com>
2021-10-23 15:33:50 -0300

    Implementando readme.md

--- estoque/views.py
+++ estoque/views.py
@@ -1,8 +1,27 @@
+import django_filters
+
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from estoque.models import EstoqueModel
 from estoque.serializers import EstoqueSerializer
+
+
+# Filter Set
+class EstoqueFilterSet(django_filters.FilterSet):
+    nome__not_contains = django_filters.CharFilter(lookup_expr='contains',
+                                                   field_name='nome',
+                                                   exclude=True)
+
+    class Meta:
+        model = EstoqueModel
+        fields = {
+            'id': ['exact', 'in'],
+            'nome': ['exact', 'contains', 'startswith'],
+            'categoria': ['exact', 'contains', 'startswith'],
+            'qtd': ['exact', 'gte', 'lte', 'gt', 'lt'],
+            'valor': ['exact', 'gte', 'lte', 'gt', 'lt'],
+        }
 
 
 # Create your views here.
@@ -16,20 +35,12 @@ class EstoqueViewSet(viewsets.ModelViewSet):
     ]
 
     # Filters options
-
-    # DjangoFilterBackend fields config,
-    filter_fields = {
-        'id': ['exact'],
-        'nome': ['exact', 'contains'],
-        'categoria': ['exact', 'contains'],
-        'qtd': ['gte', 'lte', 'gt', 'lt'],
-        'valor': ['gte', 'lte', 'gt', 'lt']
-    }
+    # Filter class
+    filter_class = EstoqueFilterSet
 
     # OrderingFilter fields config,
     ordering_fields = '__all__'
-    ordering = ['valor']
+    ordering = ['categoria', 'nome']
 
     # SearchFilter fields config,
     search_fields = ['nome', 'categoria']
-
