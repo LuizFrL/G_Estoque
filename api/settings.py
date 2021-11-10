70a15b5174122f38f5ad3bfabbf2c4a346393b82
Luiz Fernando <luiz.fernando@zerum.com>
2021-11-10 08:55:18 -0300

    :repeat: Increased token lifetime

--- api/settings.py
+++ api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -45,6 +45,10 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'django_filters'
 ]
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
+}
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
