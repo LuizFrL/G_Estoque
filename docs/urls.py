b682e7571e6ab32d5f492ce93741513f06f1861f
Luiz Fernando <luiz.fernando@zerum.com>
2021-11-16 22:04:45 -0300

    :repeat: Implements only 1 docs template

--- docs/urls.py
+++ docs/urls.py
@@ -21,6 +21,5 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
