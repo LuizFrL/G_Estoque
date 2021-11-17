480bd6e885dcedf9953ad64edecc0cc9d5d520f2
Luiz Fernando <luiz.fernando@zerum.com>
2021-11-16 22:43:31 -0300

    :arrow_up: Implement alter password of user

--- docs/urls.py
+++ docs/urls.py
@@ -21,5 +21,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
 ]
--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
-from django.http import JsonResponse
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework import serializers
 
 
@@ -11,5 +12,13 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         if validated_data.get('is_superuser'):
-            return User.objects.create_superuser(**validated_data)
-        return User.objects.create_user(**validated_data)
+            return Response(User.objects.create_superuser(**validated_data),
+                            status.HTTP_201_CREATED)
+        return Response(User.objects.create_user(**validated_data),
+                        status.HTTP_201_CREATED)
+
+    def update(self, instance, validated_data):
+        user = User.objects.get(username=instance)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
