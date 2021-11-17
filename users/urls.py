d316455c45096d978b80a5d38489c99151e1ac02
Luiz Fernando <luiz.fernando@zerum.com>
2021-11-17 16:20:57 -0300

    :repeat: Solve authorization doc error

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,23 @@
 from django.urls import path, include
-from rest_framework import routers
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import routers, status
+from rest_framework_simplejwt.serializers import TokenRefreshSerializer
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 from users.views import UserViewSet
 
+decorated_token = \
+    swagger_auto_schema(
+        method='post',
+        responses={status.HTTP_201_CREATED: TokenRefreshSerializer}
+    )(TokenObtainPairView.as_view())
 
 router = routers.DefaultRouter()
 router.register(r"user", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path('api/token/', TokenObtainPairView.as_view(),
+    path('api/token/', decorated_token,
          name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(),
          name='token_refresh'),
