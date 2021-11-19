016d7d37b2db0e7025f457dc351241276bcacfae
Luiz Fernando <luiz.fernando@zerum.com>
2021-11-18 21:12:45 -0300

    :arrow_up: Implement change password

--- users/views.py
+++ users/views.py
@@ -1,11 +1,23 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets, response
+from rest_framework import viewsets
 from rest_framework import permissions
 from users.serializers import UserSerializer
+
+
+class IsAdminOrPatchRequest(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return bool(
+            request.method == 'PATCH' and request.user.is_authenticated
+            or
+            request.user and request.user.is_superuser
+        )
 
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAdminUser]
+    permission_classes = [IsAdminOrPatchRequest]
+
+    lookup_field = "username"
